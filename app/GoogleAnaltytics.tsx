e62198d01b644dc749639d01e980932b0b0ac64e
bra11y <bryanonyen.inc@gmail.com>
2026-05-06 16:41:45 +0100

    chore: move GA measurement ID to env

--- app/GoogleAnaltytics.tsx
+++ app/GoogleAnaltytics.tsx
@@ -6,14 +6,14 @@ const GoogleAnaltytics = () => {
     <>
       <Script
         strategy='lazyOnload'
-        src='https://www.googletagmanager.com/gtag/js?id=G-M761KEXHSX'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
       ></Script>
       <Script id='ga' strategy='lazyOnload'>
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-M761KEXHSX');`}
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
       </Script>
     </>
   );
